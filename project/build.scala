c9120d009221662152444527fe7d7835ab2e0f04
Eugene Burmako <xeno.by@gmail.com>
2014-03-18 10:06:24 +0100

    2.11.0-RC2

--- project/build.scala
+++ project/build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object build extends Build {
   lazy val sharedSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.11.0-RC1",
+    scalaVersion := "2.11.0-RC2",
     crossVersion := CrossVersion.full,
     version := "2.0.0-SNAPSHOT",
     organization := "org.scalamacros",
