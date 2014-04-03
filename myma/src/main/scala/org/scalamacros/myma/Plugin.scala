560853c7ea0c1fbfb691d839ed9a71cd2ff39e15
Eugene Burmako <xeno.by@gmail.com>
2014-04-03 10:57:22 +0200

    introduces mygration manager

---
+++ myma/src/main/scala/org/scalamacros/myma/Plugin.scala
@@ -0,0 +1,48 @@
+package org.scalamacros.myma
+
+import scala.tools.nsc.{Global, Phase, SubComponent}
+import scala.tools.nsc.plugins.{Plugin => NscPlugin, PluginComponent => NscPluginComponent}
+
+class Plugin(val global: Global) extends NscPlugin {
+  import global._
+
+  val name = "myma"
+  val description = "My migration analyzer"
+  val components = List[NscPluginComponent](PluginComponent)
+
+  private object PluginComponent extends NscPluginComponent {
+    import global._
+    val global = Plugin.this.global
+
+    override val runsAfter = List("typer")
+    val phaseName = "myma"
+
+    def newPhase(prev: Phase) = new StdPhase(prev) {
+      def apply(unit: CompilationUnit) {
+        lazy val whitelist: Set[String] = {
+          // TODO: looks like using JAR resources requires closing and reopening SBT when the resource changes
+          // val resourceStream = getClass.getResourceAsStream("/whitelist.conf")
+          // if (resourceStream == null) abort("couldn't load whitelist.conf")
+          // io.Source.fromInputStream(resourceStream).getLines.toSet
+          val resourcePath = System.getProperty("myma.whitelist.conf")
+          if (resourcePath == null) abort("couldn't load whitelist.conf")
+          io.Source.fromFile(resourcePath).getLines.toSet
+        }
+        lazy val deps: Map[Symbol, Tree] = {
+          def isTrulyInternal(sym: Symbol): Boolean = {
+            val doesntComeFromApi = !sym.allOverriddenSymbols.exists(sym => sym.fullName.startsWith("scala.reflect.api"))
+            val doesntComeFromMacros = !sym.allOverriddenSymbols.exists(sym => sym.fullName.startsWith("scala.reflect.macros"))
+            doesntComeFromApi && doesntComeFromMacros
+          }
+          val deps = unit.body.collect{ case tree if tree.hasSymbol => (tree.symbol, tree) }.toMap
+          val relevant = deps.filterKeys(sym => !sym.isPackage && !sym.isModule)
+          val internal = relevant.filterKeys(_.fullName.startsWith("scala.reflect.internal."))
+          val trulyInternal = internal.filterKeys(isTrulyInternal)
+          trulyInternal
+        }
+        val unauthorized = deps.filterKeys(dep => !whitelist.contains(dep.fullName))
+        unauthorized.foreach{ case (dep, usage) => unit.error(usage.pos, s"Usage of unauthorized API: ${dep.fullName}") }
+      }
+    }
+  }
+}
