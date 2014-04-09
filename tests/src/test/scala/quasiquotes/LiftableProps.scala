ee2fffd22e4af84fdfaec32c8e89264a2abf2d31
Eugene Burmako <xeno.by@gmail.com>
2014-04-09 23:30:57 +0200

    backports https://github.com/scala/scala/pull/3671

--- tests/src/test/scala/quasiquotes/LiftableProps.scala
+++ tests/src/test/scala/quasiquotes/LiftableProps.scala
@@ -164,4 +164,14 @@ object LiftableProps extends QuasiquoteProperties("liftable") {
     val right3: Either[Int, Int] = Right(1)
     assert(q"$right3" ≈ q"scala.util.Right(1)")
   }
+
+  property("lift xml comment") = test {
+    def Liftable[T](f: T => Tree): Liftable[T] = {
+      new Liftable[T] { def apply(value: T): Tree = f(value) }
+    }
+    implicit val liftXmlComment = Liftable[xml.Comment] { comment =>
+      q"new _root_.scala.xml.Comment(${comment.commentText})"
+    }
+    assert(q"${xml.Comment("foo")}" ≈ q"<!--foo-->")
+  }
 }
