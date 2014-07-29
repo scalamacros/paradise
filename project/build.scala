610ff41afc4f624d8737a8b86fe23e18776a5624
Eugene Burmako <xeno.by@gmail.com>
2014-07-29 09:11:45 +0200

    2.11.3-SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object build extends Build {
   lazy val sharedSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.11.2-SNAPSHOT",
+    scalaVersion := "2.11.3-SNAPSHOT",
     crossVersion := CrossVersion.full,
     version := "2.1.0-SNAPSHOT",
     organization := "org.scalamacros",
