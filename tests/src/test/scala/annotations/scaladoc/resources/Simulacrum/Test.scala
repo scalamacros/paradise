1a1d6d555998b3b8d6633b0761a8b36c7a7eea65
Eugene Burmako <xeno.by@gmail.com>
2015-02-01 09:05:59 +0100

    fixes non-expansion of scaladoc-annotated definitions

--- tests/src/test/scala/annotations/scaladoc/resources/Simulacrum/Test.scala
+++ tests/src/test/scala/annotations/scaladoc/resources/Simulacrum/Test.scala
@@ -5,3 +5,12 @@ import scala.language.higherKinds
 @simulacrum trait Applicative[F[_]] {
   def pure[A](x: A): F[A]
 }
+
+class Test {
+  @simulacrum def no1 = ???
+  /** 1 */ @simulacrum def yes1 = ???
+
+  class ann extends scala.annotation.StaticAnnotation
+  @ann def no2 = ???
+  /** 2 */ @ann def yes2 = ???
+}
