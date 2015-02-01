cc34b5ab7abc298ce8bea6befaeefed79212c545
Eugene Burmako <xeno.by@gmail.com>
2015-02-01 09:05:59 +0100

    fixes non-expansion of scaladoc-annotated definitions

--- tests/src/main/scala/simulacrum.scala
+++ tests/src/main/scala/simulacrum.scala
@@ -5,6 +5,7 @@ import scala.annotation.StaticAnnotation
 object simulacrumMacro {
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
+    if (annottees.head.tree.getClass.getName.contains("DocDef")) c.abort(c.enclosingPosition, "annottee is a DocDef")
     c.Expr[Any](Block(annottees.map(_.tree).toList, Literal(Constant(()))))
   }
 }
