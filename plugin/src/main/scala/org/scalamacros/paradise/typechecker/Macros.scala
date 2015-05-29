a526169c074372ef2e21556c509d57214ab91130
Eugene Burmako <xeno.by@gmail.com>
2015-05-29 14:52:30 +0200

    backports 7f1f2c6061

--- plugin/src/main/scala/org/scalamacros/paradise/typechecker/Macros.scala
+++ plugin/src/main/scala/org/scalamacros/paradise/typechecker/Macros.scala
@@ -5,7 +5,32 @@ trait Macros {
   self: Analyzer =>
 
   import global._
+  import definitions._
   import scala.language.reflectiveCalls
+  import scala.reflect.internal.Flags._
+
+  override def typedMacroBody(typer: Typer, ddef: DefDef): Tree = {
+    val result = invokeSuper("typedMacroBody", typer, ddef).asInstanceOf[Tree]
+    val clazz = typer.context.owner.owner
+    val isMacroAnnot = clazz isNonBottomSubClass AnnotationClass // NOTE: that's an approximation, see 2.11.x
+    if (ddef.name == nme.macroTransform && isMacroAnnot) {
+      val binding = loadMacroImplBinding(ddef.symbol)
+      val message =
+        "implementation restriction: macro annotation impls cannot have typetag context bounds " +
+        "(consider taking apart c.macroApplication and manually calling c.typecheck on the type arguments)"
+      val hasTags = binding.signature.exists(_ >= 0)
+      if (hasTags) { typer.context.error(ddef.pos, message); EmptyTree }
+      else result
+    } else {
+      result
+    }
+  }
+
+  private case class MacroImplBinding(signature: List[Int])
+  private def loadMacroImplBinding(macroDef: Symbol): MacroImplBinding = {
+    val superBinding = invokeSuper("loadMacroImplBinding", macroDef).asInstanceOf[{ val signature: List[Int] }]
+    MacroImplBinding(superBinding.signature)
+  }
 
   def macroExpandUntyped(typer: Typer, expandee: Tree): Option[Tree] = {
     val result = invokeSuper("macroExpand1", typer, expandee)
