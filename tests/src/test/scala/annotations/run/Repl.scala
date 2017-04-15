c95ba4ad3ad440fa26a258f7d242e64a7712957e
Eugene Burmako <xeno.by@gmail.com>
2017-04-15 07:42:59 -0700

    adapt to changes in repl

--- tests/src/test/scala/annotations/run/Repl.scala
+++ tests/src/test/scala/annotations/run/Repl.scala
@@ -1,17 +1,29 @@
 import org.scalatest.FunSuite
 
+import scala.io.AnsiColor._
 import scala.tools.nsc.Settings
 import scala.tools.nsc.interpreter._
 
 class Repl extends FunSuite {
   private def repl(code: String): String = {
-    val s = new Settings
-    s.Xnojline.value = true
-    s.usejavacp.value = false
-    s.classpath.value = sys.props("sbt.paths.tests.classpath")
-    s.plugin.value = List(sys.props("sbt.paths.plugin.jar"))
-    val lines = ILoop.runForTranscript(code, s).lines.toList
-    lines.drop(3).dropRight(2).map(_.replaceAll("\\s+$","")).mkString("\n").trim.stripSuffix("scala>").trim
+    val oldColorOk = replProps.colorOk
+    val f_colorOk = replProps.getClass.getDeclaredField("colorOk")
+    f_colorOk.setAccessible(true)
+    f_colorOk.set(replProps, false)
+    try {
+      val s = new Settings
+      s.Xnojline.value = true
+      s.usejavacp.value = false
+      s.classpath.value = sys.props("sbt.paths.tests.classpath")
+      s.plugin.value = List(sys.props("sbt.paths.plugin.jar"))
+      val lines = ILoop.runForTranscript(code, s).lines.toList
+      // TODO: I don't know why but setting replProps.colorOk to false doesn't help.
+      // I don't have time to figure out what's going on, so I'll do simple string replacement.
+      def cleanup(line: String) = line.replace(MAGENTA, "").replace(RESET, "").trim
+      lines.drop(3).dropRight(2).map(cleanup).mkString("\n").trim.stripSuffix("scala>").trim
+    } finally {
+      f_colorOk.set(replProps, oldColorOk)
+    }
   }
 
   test("precompiled macros expand") {
@@ -63,55 +75,55 @@ class Repl extends FunSuite {
       |@thingyAdhoc class Thingy
       |@thingyAdhoc class NonThingy
     """.stripMargin.trim) === """
-      |scala> import scala.language.experimental.macros
-      |import scala.language.experimental.macros
-      |
-      |scala> import scala.reflect.macros.whitebox.Context
-      |import scala.reflect.macros.whitebox.Context
-      |
-      |scala> import scala.annotation.StaticAnnotation
-      |import scala.annotation.StaticAnnotation
-      |
-      |scala>
-      |
-      |scala> object thingyAdhocMacro {
-      |  def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
-      |    import c.universe._
-      |    val toEmit = c.Expr(q"class Thingy(i: Int) { def stuff = println(i) }; object Thingy { def apply(x: Int) = new Thingy(x) }")
-      |    annottees.map(_.tree) match {
-      |      case Nil => {
-      |        c.abort(c.enclosingPosition, "No test target")
-      |      }
-      |      case (classDeclaration: ClassDef) :: Nil => {
-      |        // println("No companion provided")
-      |        toEmit
-      |      }
-      |      case (classDeclaration: ClassDef) :: (companionDeclaration: ModuleDef) :: Nil => {
-      |        // println("Companion provided")
-      |        toEmit
-      |      }
-      |      case _ => c.abort(c.enclosingPosition, "Invalid test target")
-      |    }
-      |  }
-      |}
-      |defined object thingyAdhocMacro
-      |
-      |scala>
-      |
-      |scala> class thingyAdhoc extends StaticAnnotation {
-      |  def macroTransform(annottees: Any*): Any = macro thingyAdhocMacro.impl
-      |}
-      |defined class thingyAdhoc
-      |
-      |scala>
-      |
-      |scala> @thingyAdhoc class Thingy
-      |defined class Thingy
-      |defined object Thingy
-      |
-      |scala> @thingyAdhoc class NonThingy
-      |defined class Thingy
-      |defined object Thingy
-    """.stripMargin.trim)
+scala> import scala.language.experimental.macros
+import scala.language.experimental.macros
+
+scala> import scala.reflect.macros.whitebox.Context
+import scala.reflect.macros.whitebox.Context
+
+scala> import scala.annotation.StaticAnnotation
+import scala.annotation.StaticAnnotation
+
+scala>
+
+scala> object thingyAdhocMacro {
+|   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+|     import c.universe._
+|     val toEmit = c.Expr(q"class Thingy(i: Int) { def stuff = println(i) }; object Thingy { def apply(x: Int) = new Thingy(x) }")
+|     annottees.map(_.tree) match {
+|       case Nil => {
+|         c.abort(c.enclosingPosition, "No test target")
+|       }
+|       case (classDeclaration: ClassDef) :: Nil => {
+|         // println("No companion provided")
+|         toEmit
+|       }
+|       case (classDeclaration: ClassDef) :: (companionDeclaration: ModuleDef) :: Nil => {
+|         // println("Companion provided")
+|         toEmit
+|       }
+|       case _ => c.abort(c.enclosingPosition, "Invalid test target")
+|     }
+|   }
+| }
+defined object thingyAdhocMacro
+
+scala>
+
+scala> class thingyAdhoc extends StaticAnnotation {
+|   def macroTransform(annottees: Any*): Any = macro thingyAdhocMacro.impl
+| }
+defined class thingyAdhoc
+
+scala>
+
+scala> @thingyAdhoc class Thingy
+defined class Thingy
+defined object Thingy
+
+scala> @thingyAdhoc class NonThingy
+defined class Thingy
+defined object Thingy
+""".trim)
   }
 }
