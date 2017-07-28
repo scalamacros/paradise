4e1d17bcda9e6454f9a4045838f696f5b4622e85
Eugene Burmako <eburmako@twitter.com>
2017-07-27 21:40:24 -0700

    2.12.3

--- project/build.scala
+++ project/build.scala
@@ -4,7 +4,6 @@ import Keys._
 object build extends Build {
   lazy val sharedSettings = Defaults.defaultSettings ++ Seq(
     scalaVersion := "2.12.3",
-    resolvers += "Pre-release of Scala 2.12.3" at "https://oss.sonatype.org/content/repositories/staging",
     crossVersion := CrossVersion.full,
     version := "2.1.0-SNAPSHOT",
     organization := "org.scalamacros",
